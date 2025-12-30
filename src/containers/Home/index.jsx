f2a2ce49b2cf2f86fd26d3bfe1b56c05f600baf2
diegoflausino45 <diegocflausino@gmail.com>
2025-12-30 17:17:40 -0300

    Commit graficos

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -5,37 +5,52 @@ import {
     Package,
     Users
 } from "lucide-react";
+import EpiConsumptionChart from "../../components/Graficos/EpiConsumptionChart";
+import EpiCategoryChart from "../../components/Graficos/EpiCategoryChart";
+import { epiConsumptionData, epiCategoryData } from "../../data/dashboardMock";
 
 function Home() {
     return (
-        <div className={styles.cardsGrid}>
-            <StatCard
-                title="EPIs em Estoque"
-                value={325}
-                icon={<Package />}
-                status="success"
-            />
+        <div className={styles.home}>
+            <div className={styles.cardsGrid}>
+                <StatCard
+                    title="EPIs em Estoque"
+                    value={325}
+                    icon={<Package />}
+                    status="success"
+                />
 
-            <StatCard
-                title="EPIs Vencendo"
-                value={12}
-                icon={<AlertTriangle />}
-                status="warning"
-            />
+                <StatCard
+                    title="EPIs Vencendo"
+                    value={12}
+                    icon={<AlertTriangle />}
+                    status="warning"
+                />
 
-            <StatCard
-                title="EPIs Vencidos"
-                value={5}
-                icon={<AlertTriangle />}
-                status="danger"
-            />
+                <StatCard
+                    title="EPIs Vencidos"
+                    value={5}
+                    icon={<AlertTriangle />}
+                    status="danger"
+                />
 
-            <StatCard
-                title="Funcionários sem EPI"
-                value={8}
-                icon={<Users />}
-                status="danger"
-            />
+                <StatCard
+                    title="Funcionários sem EPI"
+                    value={8}
+                    icon={<Users />}
+                    status="danger"
+                />
+            </div>
+
+            <div className={styles.chartsGrid}>
+                <div className={styles.chartCard}>
+                    <EpiConsumptionChart data={epiConsumptionData} />
+                </div>
+
+                <div className={styles.chartCard}>
+                    <EpiCategoryChart data={epiCategoryData} />
+                </div>
+            </div>
         </div>
     )
 }
